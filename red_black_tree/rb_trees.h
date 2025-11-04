788ffc35540c4b4b0a07e2836a1ffa14c4b2b70e
chepeniv <jose.olmos@atlasschool.com>
2025-11-04 17:37:55 -0600

    integrated external sourcecode into project root

--- red_black_tree/rb_trees.h
+++ red_black_tree/rb_trees.h
@@ -33,4 +33,10 @@ typedef struct rb_tree_s
 	struct rb_tree_s *right;
 } rb_tree_t;
 
+/* function signatures */
+
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
+rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value);
+
 #endif /* _RB_TREES_H_ */
